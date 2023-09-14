9d8a28a2561486edc037dd65df419d350993766a
hjpark <hjpark@rdv.ai>
2023-09-14 16:12:27 +0900

    bug fixed

--- run_nopasswd.sh
+++ run_nopasswd.sh
@@ -4,5 +4,9 @@ rm ~/.config/monitors.xml
 #PATH_NOVNC=/home/hjpark/web_vnc_view/noVNC
 #/opt/TurboVNC/bin/vncserver :9 -SecurityTypes None -geometry 1280x720 -noVNC $PATH_NOVNC
 
+CURPATH=$(pwd)
+PATH_NOVNC=$CURPATH/noVNC
+NOVNC=$PATH_NOVNC/utils/novnc_proxy
+
 /opt/TurboVNC/bin/vncserver :9 -SecurityTypes None -geometry 1280x720
-novnc --vnc localhost:5909
+$NOVNC --vnc localhost:5909
